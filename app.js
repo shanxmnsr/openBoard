acd2ab3530046c7e78bba41e4b050a45d544a536
Shania Mansoori <shaniabca2024@gmail.com>
2026-03-02 20:41:06 +0530

    Set dynamic port for Render deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();   //Initialize and server ready
 // yeh frontend k index.html ko connect/display krayegi 
 app.use(express.static("public")); 
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 let server = app.listen(port, () => {
     console.log("Listening to port" + port);
 })
